c0eb9690cd64fda598138fb3126ef3c7a65a8ff4
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-08-04 19:50:22 -0300

    First migrationn created and connections files finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
 
+import routes from './routes'
+
 const app = express()
 
-app.get('/', (req, res) => {
-  console.log('Getting some things')
-  return res.json("Hello World!")
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
